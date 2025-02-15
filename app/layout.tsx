54dbfa3630bb8a1cb607995dcb12752c67c51f67
marlissonsilva <marlyssonsilva005@gmail.com>
2025-02-15 10:50:31 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./ui/global.css";
+import { inter } from "@/app/ui/fonts";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        />
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
